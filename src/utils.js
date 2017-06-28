85960b05e76a45358370e91247e6d27f1f728441
Richard Clark <rclark@rjmetrics.com>
2017-06-28 18:10:08 -0400

    Don't invoke callback until the window is closed.

--- src/utils.js
+++ src/utils.js
@@ -4,16 +4,14 @@ import EVENT_TYPES from "./EVENT_TYPES.js";
 export function addSourceIntegration(type, callback) {
   const client = new Client();
   let callbackInvoked = false;
+  let integration;
   client.subscribe((event) => {
     if (event.type === EVENT_TYPES.CONNECTION_CREATED && event.data.type === type)  {
-      if (!callbackInvoked) {
-        callback(event.data);
-        callbackInvoked = true;
-      }
+      integration = event.data;
     } else if (event.type === EVENT_TYPES.CLOSED || event.type === EVENT_TYPES.INTEGRATION_FORM_CLOSE) {
       client.close();
       if (!callbackInvoked) {
-        callback();
+        callback(integration);
         callbackInvoked = true;
       }
     }

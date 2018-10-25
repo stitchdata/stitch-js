abf95550353ee8c21ac11da4a15199161d595a06
John Miller <john@stitchdata.com>
2018-10-25 13:27:30 -0400

    remove dist from repo

--- src/EVENT_TYPES.js
+++ src/EVENT_TYPES.js
@@ -3,11 +3,11 @@ export default Object.freeze({
   CLOSED: "closed",
   CONNECTION_CREATED: "connectionCreated",
   CONNECTION_UPDATED: "connectionUpdated",
-  ERROR_AUTHORIZING_CONNCTION: "errorAuthorizingConnection",
   ERROR_LOADING_CONNECTION: "errorLoadingConnection",
   ERROR_LOADING_INTEGRATION_TYPE: "errorLoadingIntegrationType",
   ERROR_UPGRADING_EPHEMERAL_TOKEN: "errorUpgradingEphemeralToken",
   INTEGRATION_FLOW_COMPLETED: "integrationFlowCompleted",
   INTEGRATION_FORM_CLOSE: "integrationFormClose",
-  AUTHORIZE_SUCCESS: "authorizeSuccess"
+  AUTHORIZE_SUCCESS: "authorizeSuccess",
+  AUTHORIZE_FAILURE: "authorizeFailure"
 });
